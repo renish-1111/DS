cca9a2b26aeedbfa42a82732a6ff3fadf242cd6a
renish-1111 <ponkiyarenish@gmail.com>
2023-11-03 15:14:48 +0530

    update

--- stack_with_linkedlist_commett.c
+++ stack_with_linkedlist_commett.c
@@ -1,80 +1,118 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct node {
+struct node
+{
     int data;
-    struct node* next;
+    struct node *next;
 };
 
-struct stack {
-    struct node* top;
+struct stack
+{
+    struct node *top;
     int size;
 };
 
-struct node* create_node(int data) {
-    struct node* new_node = (struct node*)malloc(sizeof(struct node));
+struct node *create_node(int data)
+{
+    struct node *new_node = (struct node *)malloc(sizeof(struct node));
     new_node->data = data;
     new_node->next = NULL;
     return new_node;
 }
 
-struct stack* create_stack() {
-    struct stack* new_stack = (struct stack*)malloc(sizeof(struct stack));
+struct stack *create_stack()
+{
+    struct stack *new_stack = (struct stack *)malloc(sizeof(struct stack));
     new_stack->top = NULL;
     new_stack->size = 0;
     return new_stack;
 }
 
-void push(struct stack* s, int data) {
-    struct node* new_node = create_node(data);
+void push(struct stack *s, int data)
+{
+    struct node *new_node = create_node(data);
     new_node->next = s->top;
     s->top = new_node;
     s->size++;
 }
 
-int pop(struct stack* s) {
-    if (s->top == NULL) {
+int pop(struct stack *s)
+{
+    if (s->top == NULL)
+    {
         printf("Stack underflow\n");
         return -1;
     }
     int data = s->top->data;
-    struct node* temp = s->top;
+    struct node *temp = s->top;
     s->top = s->top->next;
     free(temp);
     s->size--;
     return data;
 }
 
-int peek(struct stack* s) {
-    if (s->top == NULL) {
+int peek(struct stack *s)
+{
+    if (s->top == NULL)
+    {
         printf("Stack is empty\n");
         return -1;
     }
     return s->top->data;
 }
 
-int is_empty(struct stack* s) {
+int is_empty(struct stack *s)
+{
     return s->top == NULL;
 }
 
-void display(struct stack* s) {
-    struct node* curr = s->top;
-    while (curr != NULL) {
+void display(struct stack *s)
+{
+    struct node *curr = s->top;
+    while (curr != NULL)
+    {
         printf("%d ", curr->data);
         curr = curr->next;
     }
     printf("\n");
 }
 
-int main() {
-    struct stack* s = create_stack();
-    push(s, 10);
-    push(s, 20);
-    push(s, 30);
-    display(s);
-    printf("Popped element: %d\n", pop(s));
-    printf("Top element: %d\n", peek(s));
-    printf("Stack size: %d\n", s->size);
-    printf("Stack empty: %d\n", is_empty(s));
+int main()
+{
+    struct stack *s = create_stack();
+    int choice;
+    int i;
+
+    while (1)
+    {
+        printf("\nPerform operations on the stack:");
+        printf("\n1.Push the element\n2.Pop the element\n3.Peek the element\n4.Display\n5.end");
+        printf("\n\nEnter the choice: ");
+        scanf("%d", &choice);
+        switch (choice)
+        {
+        case 1:
+            printf("Enter element:");
+            scanf("%d", &i);
+            push(s, i);
+            break;
+        case 2:
+            printf("Popped element is %d\n", pop(s));
+            break;
+
+        case 3:
+            printf("Peek element id %d\n", peek(s));
+            break;
+
+        case 4:
+            display(s);
+            break;
+        case 5:
+            exit(0);
+            break;
+        }
+    }
+
     return 0;
 }
